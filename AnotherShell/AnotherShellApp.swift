ce6be7dfc0e85d64b997a837ae2f1184cb2b59dd
GGBond <ijiapu@foxmail.com>
2026-03-24 02:08:38 +0800

    Initial Commit

---
+++ AnotherShell/AnotherShellApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnotherShellApp.swift
+//  AnotherShell
+//
+//  Created by GGBond on 24/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct AnotherShellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
